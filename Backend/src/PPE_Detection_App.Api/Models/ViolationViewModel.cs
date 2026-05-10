f92c11c784e0bd44ba4ba2b6ad9d848adf5b9bd1
VUHANAM\ASUS <namvu7702@gmail.com>
2026-05-10 10:02:34 +0700

    clear

---
+++ Backend/src/PPE_Detection_App.Api/Models/ViolationViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Models
+{
+    public class ViolationViewModel
+    {
+        public long Id { get; set; }
+        public string Category_Id { get; set; } = string.Empty;
+        public string? Category_DisplayName { get; set; }
+        public int Severity_Level { get; set; }
+        public string? Color_Code { get; set; }
+        public string Image_Path { get; set; } = string.Empty;
+        public double Confidence_Score { get; set; }
+        public DateTime Detected_Time { get; set; }
+        public double Box_X { get; set; }
+        public double Box_Y { get; set; }
+        public double Box_W { get; set; }
+        public double Box_H { get; set; }
+        public byte Status { get; set; }
+        public bool Is_Deleted { get; set; }
+        public int? Employee_Id { get; set; }
+        public string? Employee_Name { get; set; }
+        public string? Employee_Code { get; set; }
+    }
+}
